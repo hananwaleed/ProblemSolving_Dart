767324a71807ea7bdbcb1f9dc91468ced7be4c87
Hanan waleed <waleedhanan81@gmail.com>
2025-04-27 22:42:50 +0300

    Add Changes

--- Beginner/main9.dart
+++ Beginner/main9.dart
@@ -5,6 +5,6 @@ void main() {
   double salary = double.parse(stdin.readLineSync()!);
   double TotalSalary = double.parse(stdin.readLineSync()!);
   double Result = (salary + (TotalSalary * 0.15));
-  print("MY NAME IS $name");
+  print("MY NAME Iss $name");
   print("TOTAL = R\$ ${Result.toStringAsFixed(2)}");
 }
